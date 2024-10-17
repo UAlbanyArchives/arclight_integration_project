39a43cab709cd16f90b85d3573775d4d80b966ed
Wiedeman <gwiedeman@albany.edu>
2024-10-17 13:06:30 -0400

    added export scripts and docs

--- scripts/extractImages.py
+++ scripts/extractImages.py
@@ -1,96 +1,59 @@
-import os, re
+import os, re, sys
 import yaml
 from subprocess import Popen, PIPE
 from pypdf import PdfReader, PdfWriter
 
 if os.name == "nt":
-	root = "\\\\Lincoln\\Library\\SPE_DAO"
+    root = "\\\\Lincoln\\Library\\SPE_DAO"
 else:
-	root = "/media/Library/SPE_DAO"
+    root = "/media/Library/SPE_DAO"
 
+def extract_images(collection_id=None):
+    for col in os.listdir(root):
+        col_path = os.path.join(root, col)
 
-for col in os.listdir(root):
-	colPath = os.path.join(root, col)
-	if os.path.isdir(colPath):
-		for objID in os.listdir(colPath):
-			objPath = os.path.join(colPath, objID, "v1")
-			metadataPath = os.path.join(objPath, "metadata.yml")
-			pdfPath = os.path.join(objPath, "pdf")
-			if os.path.exists(pdfPath):
-				jpgPath = os.path.join(objPath, "jpg")
+        # Check if collection_id is provided and matches the current collection
+        if collection_id and collection_id not in col:
+            continue  # Skip this collection if it doesn't match
 
-				pdfCount = 0
-				for pdf in os.listdir(pdfPath):
-					pdfFilePath = os.path.join(pdfPath, pdf)
-					if os.path.isfile(pdfFilePath) and pdf.lower().endswith(".pdf"):
-						pdfCount += 1
+        if os.path.isdir(col_path):
+            for obj in os.listdir(col_path):
+                objPath = os.path.join(col_path, obj, "v1")
+                metadataPath = os.path.join(objPath, "metadata.yml")
+                pdfPath = os.path.join(objPath, "pdf")
+                if os.path.exists(pdfPath):
+                    jpgPath = os.path.join(objPath, "jpg")
 
-				if pdfCount != 1:
-					raise Exception(f"ERROR: found {pdfCount} PDF files for {col}/{objID}")
-				else:
-					for pdf in os.listdir(pdfPath):
-						if pdf.lower().endswith(".pdf"):
-							filepath = os.path.join(pdfPath, pdf)
-							filename = os.path.splitext(pdf)[0]
-							#convertDir = os.path.join(jpgPath, filename)
-							convertDir = jpgPath
+                    pdfCount = 0
+                    for pdf in os.listdir(pdfPath):
+                        pdfFilePath = os.path.join(pdfPath, pdf)
+                        if os.path.isfile(pdfFilePath) and pdf.lower().endswith(".pdf"):
+                            pdfCount += 1
 
-							print (f"Processing {pdf} from {col}/{objID}...")
+                    if pdfCount != 1:
+                        raise Exception(f"ERROR: found {pdfCount} PDF files for {col}/{obj}")
+                    else:
+                        for pdf in os.listdir(pdfPath):
+                            filepath = os.path.join(pdfPath, pdf)
+                            filename = os.path.splitext(pdf)[0]
+                            #convertDir = os.path.join(jpgPath, filename)
+                            convertDir = jpgPath
 
-							with open(metadataPath, 'r') as yml_file:
-								data = yaml.safe_load(yml_file)
-							data['original_pdf'] = pdf
-							with open(metadataPath, 'w') as yml_file:
-								yaml.dump(data, yml_file)
+                            print (f"Processing {pdf} from {col}/{obj}...")
 
-							if not os.path.isdir(convertDir):
-								os.mkdir(convertDir)
+                            if not os.path.isdir(convertDir):
+                                os.mkdir(convertDir)
+                            outfile = os.path.join(convertDir, filename)
 
-							# Getting DPI for each image
-							pageDPI = {}
-							pdfimagesCmd = ["pdfimages", "-list", filepath]
-							pdfimages = Popen(pdfimagesCmd, stdout=PIPE, stderr=PIPE)
-							lineCount = -2
-							for line in pdfimages.stdout:
-								lineCount += 1
-								if lineCount > 0:
-									col = re.split("\\s+", line.decode().strip())
-									pageDPI[lineCount] = f"{col[12]}x{col[13]}"
+                            pdfimagesCmd = ["pdftoppm", filepath, outfile, "-jpeg"]
+                            pdfimages = Popen(pdfimagesCmd, stdout=PIPE, stderr=PIPE)
+                            stdout, stderr = pdfimages.communicate()
+                            
 
-							# convert to images
-							pageOrder = []
-							pdf_reader = PdfReader(filepath)
-							page_count = 0
-							page_total = len(pdf_reader.pages)
-
-							file = pdf
-
-							for page in pdf_reader.pages:
-								page_count += 1
-								if len(page.images) != 1:
-									raise ValueError(f"ERROR: During OCR prep, PDF page number {page_count} has multiple images.")
-								page_rotation = page.get('/Rotate')
-								for image in page.images:
-									ext = os.path.splitext(image.name)[1]
-									image_path = os.path.join(convertDir, f"{os.path.splitext(file)[0]}-{page_count}{ext}")
-									if os.path.isfile(image_path):
-										raise ValueError(f"ERROR: In extracting images for OCR, file already exists: {image_path}.")
-									with open(image_path, "wb") as fp:
-										fp.write(image.data)
-
-									# fix sizing
-									print (f"\tRestoring to {pageDPI[page_count]} dpi...")
-									size_cmd = ['convert', '-units', 'pixelsperinch', '-density', pageDPI[page_count], image_path, image_path]
-									if ext.lower() == "png":
-										size_cmd.insert(1, '-units pixelsperinch')
-									#print (f"\trunning {' '.join(size_cmd)}")
-									size_resp = process(size_cmd)
-									if size_resp != 0:
-										raise ValueError(f'Error resizing file {image_path}')
-
-									#address image rotation
-									if page_rotation:
-										print ("\tFixing image rotation...")
-										img = Image.open(image_path)
-										img_rotate = img.rotate(-abs(page_rotation), expand=1)
-										img_rotate.save(image_path)
+if __name__ == "__main__":
+    # Check for command-line arguments
+    if len(sys.argv) > 1:
+        collection_id_arg = sys.argv[1]
+        extract_images(collection_id=collection_id_arg)
+    else:
+        extract_images()
