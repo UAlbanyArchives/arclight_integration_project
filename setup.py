8163e8c9fe5d72a7cb2d923a13b10579092f36ab
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-03-06 14:25:27 -0500

    NOW should correctly handle different image api paths

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setuptools.setup(
     name="iiiflow",
-    version="0.2.0",
+    version="0.2.1",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="An IIIF pipeline tool using the Digital Object Discovery Storage Specification.",
