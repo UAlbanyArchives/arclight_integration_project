cbbaf28d057d5c572e6ec501c10272b6d57fc83d
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-05-21 15:42:33 -0400

    now has fallback options for manifest labels to address downstream accessibility issues

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setuptools.setup(
     name="iiiflow",
-    version="0.4.3",
+    version="0.5.0",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="An IIIF pipeline tool using the Digital Object Discovery Storage Specification.",
