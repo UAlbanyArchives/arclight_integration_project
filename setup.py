feea8bd2db880e3b35f2f9db9998cb4f60e3f4d0
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-07-01 16:21:12 -0400

    helps validate objects before rebuilding

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requirements = [
 
 setuptools.setup(
     name="iiiflow",
-    version="0.6.2",
+    version="0.6.3",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="An IIIF pipeline tool using the Digital Object Discovery Storage Specification.",
