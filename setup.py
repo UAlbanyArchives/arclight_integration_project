746505251cd04071adec18ae90a5b9e73e68ef12
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-10-22 12:58:01 -0400

    Now handles thumbnails for short videos

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setuptools.setup(
     name="iiiflow",
-    version="0.7.7",
+    version="0.7.8",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="An IIIF pipeline tool using the Digital Object Discovery Storage Specification.",
