dee57eb35f5b3766bb59c66424bfb25a626671a8
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2026-02-11 13:33:56 -0500

    Fixed av transcription bug not accounting for missing directories

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setuptools.setup(
     name="iiiflow",
-    version="0.8.3",
+    version="0.8.4",
     author="Gregory Wiedeman",
     author_email="gwiedeman@albany.edu",
     description="An IIIF pipeline tool using the Digital Object Discovery Storage Specification.",
